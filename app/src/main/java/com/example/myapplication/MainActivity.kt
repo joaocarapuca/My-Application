f88715c56c34f57418c5ce76db82d52ff90ec14d
João Carapuça <joaocarapuca12@gmail.com>
2025-07-09 00:25:46 +0100

    Updated build.gradle.kts

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import com.example.myapplication.routes.AppNavHost
+import com.example.myapplication.routes.AppNavHosts
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            AppNavHost(navController = navController)
+            AppNavHosts(navController = navController)
         }
     }
 }
